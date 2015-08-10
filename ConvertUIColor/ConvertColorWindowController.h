931f9e8973e38ed651d5185fe96221a3b76c3b48
mont <otera5615@yahoo.co.jp>
2015-08-11 01:07:11 +0900

    UIColor変換用のWindowを作成

--- ConvertUIColor/ConvertColorWindowController.h
+++ ConvertUIColor/ConvertColorWindowController.h
@@ -10,4 +10,13 @@
 
 @interface ConvertColorWindowController : NSWindowController
 
+// 16進数入力用TextField
+@property (weak) IBOutlet NSTextField *hexTextField;
+// 変換結果出力用TextField
+@property (weak) IBOutlet NSTextField *uicolorTextField;
+
+// Convertボタン押下時
+- (IBAction)convert:(id)sender;
+// Clearボタン押下時
+- (IBAction)clear:(id)sender;
 @end
--- ConvertUIColor/ConvertColorWindowController.m
+++ ConvertUIColor/ConvertColorWindowController.m
@@ -20,4 +20,9 @@
     // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
 }
 
+- (IBAction)convert:(id)sender {
+}
+
+- (IBAction)clear:(id)sender {
+}
 @end
