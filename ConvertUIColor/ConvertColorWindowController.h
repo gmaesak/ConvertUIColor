483918fc4ec7fcda7a6aeec2d49b13855ab333c5
mont <otera5615@yahoo.co.jp>
2015-08-11 00:57:11 +0900

    ConvertColorWindowControllerを追加

---
+++ ConvertUIColor/ConvertColorWindowController.h
@@ -0,0 +1,13 @@
+//
+//  ConvertColorWindowController.h
+//  ConvertUIColor
+//
+//  Created by mont on 2015/08/11.
+//  Copyright (c) 2015年 gmaesak. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ConvertColorWindowController : NSWindowController
+
+@end
---
+++ ConvertUIColor/ConvertColorWindowController.m
@@ -0,0 +1,23 @@
+//
+//  ConvertColorWindowController.m
+//  ConvertUIColor
+//
+//  Created by mont on 2015/08/11.
+//  Copyright (c) 2015年 gmaesak. All rights reserved.
+//
+
+#import "ConvertColorWindowController.h"
+
+@interface ConvertColorWindowController ()
+
+@end
+
+@implementation ConvertColorWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
